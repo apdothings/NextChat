be0d6954a4dd9b98a3faabd726716e100c765c05
kohalex9 <kohalex9@gmail.com>
2017-09-08 16:44:12 +0800

    add new signinvc with respective outlets

---
+++ SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.swift
+//  NextChat
+//
+//  Created by Alex Koh on 08/09/2017.
+//  Copyright © 2017 Salem Abdulla. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
