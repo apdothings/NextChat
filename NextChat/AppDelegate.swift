18353c093f19c2d70d29b9000b0895acb8413de8
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-08 17:41:23 +0800

    sign up vc partial done

--- NextChat/AppDelegate.swift
+++ NextChat/AppDelegate.swift
@@ -18,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let authStoryboard = UIStoryboard(name: "SignUpStoryboard", bundle: Bundle.main)
+        let SignInVC = authStoryboard.instantiateViewController(withIdentifier: "SignUpViewController")
+        
+        window?.rootViewController = SignInVC
+        window?.makeKeyAndVisible()
+        
         FirebaseApp.configure()
         
         return true
