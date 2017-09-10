90cc530fef4fba5f36be8218f9cd65beeea14624
kohalex9 <kohalex9@gmail.com>
2017-09-10 13:51:17 +0800

    Completed SignUpVC

--- SignInViewController.swift
+++ SignInViewController.swift
@@ -15,13 +15,51 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton! {
+        didSet {
+            signInBtn.addTarget(self, action: #selector(signInBtnTapped), for: .touchUpInside)
+        }
+    }
+    @IBOutlet weak var signUpBtn: UIButton! {
+        didSet {
+            signUpBtn.addTarget(self, action: #selector(signUpBtnTapped), for: .touchUpInside)
+        }
+    }
+    
+    func signInBtnTapped() {
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        if emailTextField.text == "" {
+            createErrorVC(title: "Empty email fill", message: "Please input valid email")
+            return
+        } else if passwordTextField.text == "" {
+            createErrorVC(title: "Empty password fill", message: "Please input valid password fill")
+            return
+        }
+        
+        
+        
+        
+    }
+    
+    func signUpBtnTapped() {
+        
+    }
+    
+    func createErrorVC(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //check if there is any user logged in
-        //If yes, we go ChatViewController which is inside ChatViewStoryboard
+        //If yes, we go ChatViewController which is inside ChatViewStoryboard (different storyBoard)
         if Auth.auth().currentUser != nil {
             let ChatViewStoryboard = UIStoryboard(name: "ChatViewStoryboard", bundle: Bundle.main)
             let chatVC = ChatViewStoryboard.instantiateViewController(withIdentifier: "TheChatViewController")
@@ -30,4 +68,6 @@ class SignInViewController: UIViewController {
             window.makeKeyAndVisible()
         }
     }
+    
+    
 }
