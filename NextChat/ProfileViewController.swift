747325740f6c9258b6fc38fd3331db0df2c00845
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-11 11:17:19 +0800

    add user class, improve edit button function,

--- NextChat/ProfileViewController.swift
+++ NextChat/ProfileViewController.swift
@@ -12,40 +12,81 @@ import FirebaseStorage
 
 
 class ProfileViewController: UIViewController {
-
-   
-   
+    
+    
+    var selectedUser : Users?
+    var ref : DatabaseReference!
+    var isEdit: Bool = true
+    
     
     @IBOutlet weak var userNameTextField: UITextField!
     
+    @IBOutlet weak var editButton: UIButton!{
+        
+        didSet{
+            editButton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+            
+        }
+        
+    }
     
     @IBAction func LogOutButton(_ sender: Any) {
         
         
     }
     
+    func editButtonTapped(){
+        
+        if isEdit == true {
+            
+            userNameTextField.isUserInteractionEnabled = true
+            editButton.setTitle("Done", for: .normal)
+            isEdit = false
+        }
+        else  {
+            
+            ref = Database.database().reference()
+            guard let email = selectedUser?.email,
+            let id = selectedUser?.usersID
+            
+            
+            else{return}
+            
+            let post : [String : Any] = ["emailAddress" : email]
+            
+            //dig paths to reach a specific student
+            ref.child("users").child("\(id)").updateChildValues(post)
+            editButton.setTitle("Edit", for: .normal)
+            isEdit = true
+            
+            userNameTextField.isUserInteractionEnabled = false
+            
+        }
+        
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ NextChat/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  NextChat
+//
+//  Created by Habib Zarrin Chang Fard on 11/09/2017.
+//  Copyright © 2017 Salem Abdulla. All rights reserved.
+//
+
+import Foundation
+
+class Users{
+    var usersID : Int = 0
+    var email : String = ""
+    
+    init(randomID1: Int,anEmail: String){
+        usersID = randomID1
+        email = anEmail
+        
+        
+        
+    }
+}
